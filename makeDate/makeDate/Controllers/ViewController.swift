f3304b2bc7eb673442b829a4693bf85c748783fa
mohammed <mdrame113@gmail.com>
2019-12-16 18:49:01 -0800

    Restart project: Change Ui:

--- makeDate/makeDate/Controllers/ViewController.swift
+++ makeDate/makeDate/Controllers/ViewController.swift
@@ -8,154 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
     
-    // IBOutlets
-    @IBOutlet weak var userNameLogIn: LoginPageTextFields!
-    @IBOutlet weak var passWordLogIn: LoginPageTextFields!
-    
-    @IBOutlet weak var baseUIVieew: CustomUIView!
-    @IBOutlet weak var secondBaseUIView: CustomUIView!
-    
-    @IBOutlet weak var fingerPrintOutlet: UIButton!
-    @IBOutlet weak var logInLableOutlet: UILabel!
-    
-    @IBOutlet weak var registerButtonOutlet: UIButton!
-    
-    
-    // Delegates stuff
-    func textfieldSDelegates() {
-        userNameLogIn.delegate = self
-        passWordLogIn.delegate = self
-    }
-    
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        unhideFingerprint()
-    }
-    
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//        hideFingerPrint()
-//
-//    }
-    
-    
-    // UIView layout stuffs
-    func uiviewCall() {
-        
-        baseUIVieew.baseUIViewlayoutStuff()
-        secondBaseUIView.secondBaseUIViewlayoutStuff()
-        
-    }
-    
-    func registerButtonLayout() {
-        registerButtonOutlet.layer.cornerRadius = 5
-    }
-    
-    
-    // uitextfield layout stuffs
-    func textFieldCall() {
-        userNameLogIn.textFieldplaceHolder(placeHolderText: "User", color: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3008637764))
-        passWordLogIn.textFieldplaceHolder(placeHolderText: "Pass", color: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.296737456))
-    }
-    
-    
-    
-    // Hide finger print when view load
-    func hideFingerPrint() {
-        
-        fingerPrintOutlet.isHidden = true
-        logInLableOutlet.isHidden = true
-           
-            UIView.animate(withDuration: 0.5) {
-                self.fingerPrintOutlet.center.y = 700
-                
-            }
-    }
-    
-    
-    
-    // UNHide fingerprint if user start to edit the password
-    func unhideFingerprint() {
-        
-                fingerPrintOutlet.isHidden = false
-                logInLableOutlet.isHidden = false
-            
-            UIView.animate(withDuration: 0.5) {
-                self.fingerPrintOutlet.center.y = 400
-     
-        
-    }
-    }
-    
-    
-//
-//    func userNameLogInFunctionCalls(view: UIView) {
-//        // edit userName textield here
-//        userNameLogIn.textFieldplaceHolder(placeHolderText: "User Name", color: #colorLiteral(red: 0.8821972863, green: 0.9148316062, blue: 0, alpha: 0.5))
-//
-//    }
-//
-//    func passWordLogInFunctionCalls(view: UIView) {
-//           // edit passWord textfield here
-//        passWordLogIn.textFieldplaceHolder(placeHolderText: "PassWord", color: #colorLiteral(red: 0.8821972863, green: 0.9148316062, blue: 0, alpha: 0.5) )
-//
-//       }
-    
+
+              
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        uiviewCall()
-        // textFieldOutlet call
-        textFieldCall()
-        
-        hideFingerPrint()
-        registerButtonLayout()
-        
-        gesture()
-//        // calling and setting textfield as a paremeter so layers  in the function can access it
-//        userNameLogInFunctionCalls(view: userNameLogIn)
-//        passWordLogInFunctionCalls(view: passWordLogIn)
-//        
-//     
-        
-        
-      textfieldSDelegates()
-        // Do any additional setup after loading the view.
     }
     
     
     
-    // Actions
-    @IBAction func logInpress(_ sender: UIButton) {
-        // verify user and perform segue if successful. else shake textfield and pup an error message
+  // ---------------------------------------------//
+ // - - - - - - - - - IBOutlets - - - - - - - - -//
+// ---------------------------------------------//
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var loginButtonOutlet: UIButton!
+    @IBOutlet weak var registerButtonOutlet: UIButton!
+    
+    
+    
+    
+    
+    
+  // ---------------------------------------------//
+ // - - - - - - - - - IBActions - - - - - - - - -//
+// ---------------------------------------------//
+    
+    @IBAction func thumpPress(_ sender: UIButton) {
+        // 1. Pop login and logout button from under thumpress
         
+        //2. change thumPress Image ( Darker )
         
     }
     
     
-    @IBAction func register(_ sender: UIButton) {
-        performSegue(withIdentifier: "toRegister", sender: self)
+    @IBAction func login(_ sender: UIButton) {
+        // 1. Ask user to get access to Keychain to get faceId
+        
+        // 2. Compare faceId with current face scan .
+        
+        // 3. If successful send user to profile page
+        
+        // 4. Else Vibrate phone and create custom error notification
+        
     }
-    
-    // Unwind from register page
-@IBAction func unwindToVC1(segue:UIStoryboardSegue) { }
-    
-    
-    
-    func gesture() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tap))
-       secondBaseUIView.addGestureRecognizer(tapGesture)
-    
-    }
-    
-    @objc func tap() {
-//       unhideFingerprint()
-        self.view.endEditing(true)
-    }
-    
-    
     
     
     
--- makeDate/makeDate/Views/backgroundView/CustomUIView.swift
+++ makeDate/makeDate/Views/backgroundView/CustomUIView.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class CustomUIView: UIView {
 
-
-
         // Programmatic Initialization
            override init(frame: CGRect) {
            super.init(frame: frame)
@@ -60,10 +58,44 @@ class CustomUIView: UIView {
     //              layer.shadowRadius = 10
     //              layer.shadowOpacity = 0.9
                   
-              }
+        }
     
+
     
     
     
 
 }
+
+
+//class RegisterHomeView: CustomUIView {
+//    
+//    
+//    override funct baseUIViewlayoutStuff() {
+//                    // border
+//                     layer.borderWidth = 2
+//                     layer.borderColor = #colorLiteral(red: 0.94375211, green: 0.9184934497, blue: 0.1899807155, alpha: 1)
+//                     layer.cornerRadius = 9
+//        
+//    }
+//    
+//    func secondBaseUIViewlayoutStuff() {
+//                  
+//                  // border
+//                  layer.borderWidth = 2
+//                  layer.borderColor = #colorLiteral(red: 0.4714163542, green: 0.4614343047, blue: 0.09063761681, alpha: 1)
+//                  layer.cornerRadius = 5
+//                  
+//                  // shadow
+//    //              layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+//    //              layer.shadowOffset = CGSize(width: 0, height: 2)
+//    //
+//    //              layer.shadowRadius = 10
+//    //              layer.shadowOpacity = 0.9
+//                  
+//        }
+//    
+//    
+//    
+//    
+//}
